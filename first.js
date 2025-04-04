8a71253d682f71edf1d9ebfd5be14ac74806bf37
pawan <pawankhamdak@gmail.com>
2025-04-04 19:43:11 +0545

    also change

--- first.js
+++ first.js
@@ -1,8 +1,24 @@
-let company=['bloomberg','microsoft','uber','Ibm','Netflix'];
-console.log(company);
-company.shift();
-console.log(company);
-company.splice(1,1,'ola');
-console.log(company);
-company.push('Amazon');
-console.log(company);
+let marks=[40,30,70.90,95,100];
+let avbnty=marks.filter((val)=>{
+if (val>=90){
+    return val;
+}
+});
+console.log("the students who score abv 90 :",avbnty);
+
+let n= prompt("enter the numper");
+let arr=[];
+for (let i=0; i<=n; i++){
+    arr.push(i);
+}
+console.log(arr);
+ 
+let sum=arr.reduce((res,curr)=>{
+return res+curr;
+})
+console.log("sum of the array values:",sum);
+
+let product=arr.reduce((res,curr)=>{
+    return res*curr;
+    })
+    console.log("product of the array values:",product);
